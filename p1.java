c692956842319eb9196ce3037dc08bdf93f923da
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2025-01-01 12:43:54 +0600

    Create p1.java

---
+++ p1.java
@@ -0,0 +1,15 @@
+public class  Main
+{
+  public static void main (String[] args) 
+  {
+  String Firstname= "Antoreep";
+    String Lastname= "Bayzid";
+   String Fullname= Firstname + " " +Lastname;
+   int num1=69;
+   int num2=71;
+   int total=num1+num2;
+    System.out.println(total);
+   System.out.println(Fullname);
+  
+  }
+}
