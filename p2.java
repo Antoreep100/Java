181e9ce5daf54694ac16b7c0964fe71e3b5df88b
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2025-01-01 14:03:16 +0600

    Create p2.java

---
+++ p2.java
@@ -0,0 +1,16 @@
+public class  Main
+{
+  public static void main (String[] args) 
+  {
+int items = 100;
+int costPerItem = 79;
+int totalCost = items * costPerItem;
+char currency = '$';
+
+// Print variables
+System.out.println("Number of items: " + items);
+System.out.println("Cost per item: " + costPerItem + currency);
+System.out.println("Total cost = " + totalCost + currency);
+  
+  }
+}
