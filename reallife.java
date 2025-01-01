f4b6686eeeb29ccf3facc6d5cc82bc6f29707942
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2025-01-01 13:26:29 +0600

    Create reallife.java

---
+++ reallife.java
@@ -0,0 +1,19 @@
+public class  Main
+{
+  public static void main (String[] args) 
+  {
+String studentName = "Antoreep Bin Bayzid";
+Long studentID = 2233081490L;
+int studentAge = 23;
+Double studentFee = 75.25;
+char studentGrade = 'A';
+
+// Print variables
+System.out.println("Student name: " + studentName);
+System.out.println("Student id: " + studentID);
+System.out.println("Student age: " + studentAge);
+System.out.println("Student fee: " + studentFee);
+System.out.println("Student grade: " + studentGrade);
+  
+  }
+}
