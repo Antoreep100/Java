0ba8f688c83daebd279ed11386d749aad156df01
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2025-02-02 22:47:43 +0600

    Create pi.java

---
+++ pi.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+    Scanner input= new Scanner(System.in);
+    
+      System.out.println("Enter PI number:");
+      float pi=input.nextFloat();
+      System.out.println("Pi number is" + pi);
+  }
+}
