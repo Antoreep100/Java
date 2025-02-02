f142fc4f7c06a837f3f3046de655de23ac650a4e
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2025-02-02 22:23:42 +0600

    Create 2D Arrays.java

---
+++ 2D Arrays.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+      int [][] numbers=new int [5][4];
+      numbers[2][3]=4;
+      System.out.println(Arrays.deepToString(numbers));
+  }
+}
