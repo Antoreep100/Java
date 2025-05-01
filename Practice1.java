e58363cf1840560d057abc650833b1e9b95b91c9
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2025-05-02 01:23:09 +0600

    Create Practice1.java

---
+++ Practice1.java
@@ -0,0 +1,14 @@
+
+
+class Main {
+    public static void main(String[] args) {
+ String Name= "Antoreep Bin Bayzid";
+Long ID = 2233081490L;
+ int Age = 25;
+ float fees =233.445f;
+ System.out.println("Name:" +Name);
+ System.out.println("ID:" +ID);
+ System.out.println("Age:" +Age);
+  System.out.println("fees:" +fees); 
+    }
+}
