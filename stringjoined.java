e53205f244af91d4cc809274d042e975bf0307dd
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2025-05-01 22:44:22 +0600

    Create stringjoined.java

---
+++ stringjoined.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        String greet = "Hello!";
+        System.out.println("First String: " + greet);
+        String greet2 = "Programming";
+         System.out.println("Second String: " + greet2);
+         
+         String joinedprogram = greet.concat(greet2);
+         System.out.println("Program : " + joinedprogram);
+    }
+    }
