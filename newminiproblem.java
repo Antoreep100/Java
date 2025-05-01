ab95880038a64ad76d41e13903e2959fb557cf5d
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2025-05-01 17:42:01 +0600

    Create newminiproblem.java

---
+++ newminiproblem.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class Main{
+    public static void main(String [] args)
+    {
+        int sum2=0;
+        int max =0;
+        int sum=0;
+        int[][] arr = new int[3][4];
+        System.out.println("Enter " + arr.length + " the integer value:");
+        Scanner input = new Scanner(System.in);
+        for (int i = 0;i<arr.length ; i++){
+        for (int j = 0;j<arr[i].length ; j++)
+        {
+            arr[i][j]=input.nextInt();
+        }
+        }
+         System.out.println("Array:" );
+        for(int i = 0;i< arr.length;i++){
+            
+        
+        System.out.println("Array:" + Arrays.toString(arr[i]));
+        }
+        for(int i = 0;i<arr.length ; i++){
+        for (int j = 0;j<arr[i].length ; j++)
+        {
+            sum+=arr[i][j];
+        }
+        }
+        System.out.println("The summation of Array is:" + sum);
+         for(int i = 0; i < arr.length ; i++){
+          for (int j = 0;j<arr[i].length ; j++)
+        {
+            if(arr[i][j]> max)
+            {
+                max=arr[i][j];
+            }
+        }
+        }
+         System.out.println("The max of Array is:" +max);
+         
+            for(int i = 0;i<arr.length ; i++){
+        for (int j = 0;j<arr[i].length ; j++)
+        {
+            sum2+=arr[1][j];
+        }
+        }
+        System.out.println("The summation of 2nd row Array is:" + sum2);
+    }
+}
